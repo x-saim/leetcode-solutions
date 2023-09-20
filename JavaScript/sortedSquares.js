930d92b9a62195d9b009f644edd1a113395ebedb
Saim Amir <saimxamir@gmail.com>
2023-09-20 12:21:49 -0400

    add two pointer method implementation

--- JavaScript/sortedSquares.js
+++ JavaScript/sortedSquares.js
@@ -1,5 +1,3 @@
-nums = [-4, -1, 0, 3, 10];
-
 //Output: [0,1,9,16,100]
 
 const sortedSquares = (array) => {
@@ -12,5 +10,29 @@ const sortedSquares = (array) => {
   });
 };
 
-console.log(sortedSquares(nums));
-console.log(sortedSquares([-7, -3, 2, 3, 11]));
+//Two Pointer Method
+const sortedSquaresTwoPointers = (array) => {
+  let left = 0;
+  let right = array.length - 1;
+  const result = [];
+
+  //Need to iterate backwards to maintain non-decreasing order.
+  for (let i = array.length - 1; i >= 0; i--) {
+    if (Math.abs(array[left]) < Math.abs(array[right])) {
+      result[i] = array[right] ** 2;
+      right--;
+    } else {
+      result[i] = array[left] ** 2;
+      left++;
+    }
+  }
+  return result;
+};
+
+nums = [-4, -1, 0, 3, 10];
+
+// console.log(sortedSquares(nums));
+// console.log(sortedSquares([-7, -3, 2, 3, 11]));
+
+console.log(sortedSquaresTwoPointers(nums));
+console.log(sortedSquaresTwoPointers([-7, -3, 2, 3, 11]));
